793465964ad640f268f86c6498d249de14ca3b2e
Jean Mark Kouassi <jeanmark.kouame@gmail.com>
2021-10-23 21:12:14 +0000

    jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div style = {{border: "solid 1px black", maxWidth:"100 vw"}}>
+
+        <h1 className="title red">JSX</h1>
+
+        <br/>
+
+        <img src={require('./imageInSrc.jpg') } alt="imageSrc" />
+        
+
+        <br/>
+      
+        <img src='/imageInPublic.jpg' alt="imagepublic" />
+
+      </div>
+
+  <vidéo width="320" height="240" contrôles>
+
+  <source src="maVidéo.mp4" type="video/mp4" />
+
+  </vidéo>
     </div>
   );
 }
